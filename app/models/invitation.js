6497e561c35704bbd88550cca4d6855682809a23
Yandiro <tuliobbs@gmail.com>
2020-05-29 16:47:01 +1000

    Homework4: deleting controllers folder; handling index route in routes/index.js (deleted the controller); validation of invitation model in the model; @action deleteContact in routes/admin/contacts.js; @action deleteInvitation in routes/admin/invitations.js; adding del button to admin/invitations; adding del button to admin/contacts; refactoring of templates/index.hbs to bind directly to model (instead of controller);

--- app/models/invitation.js
+++ app/models/invitation.js
@@ -1,5 +1,14 @@
 import Model, { attr } from '@ember-data/model';
 
+import { match, not } from '@ember/object/computed';
+
+
 export default class InvitationModel extends Model {
   @attr('string') email;
+
+  @match('email', /^.+@.+\..+$/)
+  isValid;
+
+  @not('isValid')
+  isDisabled;
 }
--- app/routes/admin/invitations.js
+++ app/routes/admin/invitations.js
@@ -1,9 +1,20 @@
 import Route from '@ember/routing/route';
+import { action } from '@ember/object';
+
 
 export default class AdminInvitationsRoute extends Route {
 
   model() {
     return this.store.findAll('invitation');
+  }
+
+  @action
+  deleteInvitation(invitationModel) {
+    let confirmation = confirm('Are you sure?');
+
+    if (confirmation) {
+      invitationModel.destroyRecord();
+    }
   }
   
 }
